39b900bad5eb9c1283b20d319244719e43d10843
JarethUWG <jbatty1@my.westga.edu>
2024-09-22 19:43:11 -0400

    UI For Adding Food
    Set the basic UI for adding food to the project which will be associated with a data class later on.

--- code/Project 1/JavaFX Sample/src/edu/westga/cs1302/project_1/view/MainWindow.java
+++ code/Project 1/JavaFX Sample/src/edu/westga/cs1302/project_1/view/MainWindow.java
@@ -1,10 +1,34 @@
 package edu.westga.cs1302.project_1.view;
 
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.ListView;
+
+import javafx.scene.control.ComboBox;
+
+import javafx.fxml.FXML;
+
 /** Codebehind for the Main Window of the application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
+	
+	 	@FXML
+	    private TextField name;
+
+	    @FXML
+	    private ListView<?> pantry;
+
+	    @FXML
+	    private ComboBox<?> type;
+
+	    @FXML
+	    void addFood(ActionEvent event) {
+	    	
+	    }
 
 }
