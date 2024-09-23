01d742f6625e51dc81a7f733879248aad30bf058
JarethUWG <jbatty1@my.westga.edu>
2024-09-22 20:28:56 -0400

    Business Logic for Storing a Food
    Added the data class which stores food items to be integrated with the UI.

---
+++ code/Project 1/JavaFX Sample/src/edu/westga/cs1302/project_1/model/Food.java
@@ -0,0 +1,83 @@
+package edu.westga.cs1302.project_1.model;
+
+/** Stores and manages information for a single food
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class Food {
+	private String name;
+	private String type;
+	private int quantity;
+	
+	/** Create a new food with the specified name and type
+	 * 
+	 * @precondition name != null && name.length() >= 1 && type != null
+	 * @postcondition getName() == name && getType() == type && quantity == 0
+	 * 
+	 * @param name the name of the new food
+	 * @param type the type of the new food
+	 */
+	public Food(String name, String type) {
+		if (name == null) {
+			throw new IllegalArgumentException("Name must be provided.");
+		}
+		if (name.length() < 1) {
+			throw new IllegalArgumentException("Name must have at least 1 character.");
+		}
+		if (type == null) {
+			throw new IllegalArgumentException("Type must be provided.");
+		}
+		this.name = name;
+		this.type = type;
+		this.quantity = 0;
+	}
+	
+	/** Updates the value of quantity by a specified amount
+	 * 
+	 * @precondition inputValue + quantity >= 0
+	 * @postcondition this.quantity += inputValue
+	 * 
+	 * @param inputValue the amount to change quantity by
+	 */
+	public void updateQuantity(int inputValue) {
+		if ((inputValue + this.quantity) < 0) {
+			throw new IllegalArgumentException("Cannot decrease quantity below zero.");
+		}
+		this.quantity += inputValue;
+	}
+	
+	/** Creates a formatted string containing the name and quantity of the food
+	 * 
+	 * @return a formatted string containing the name and quantity of the food
+	 */
+	public String toString() {
+		String text = this.name + "-" + this.quantity;
+		return text;
+	}
+	
+	/** Returns the name of the food
+	 * 
+	 * @return the name of the food
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/** Returns the type of the food
+	 * 
+	 * @return the type of the food
+	 */
+	public String getType() {
+		return this.type;
+	}
+	
+	/** Returns the quantity of the food
+	 * 
+	 * @return the quantity of the food
+	 */
+	public int getQuantity() {
+		return this.quantity;
+	}
+	
+}
