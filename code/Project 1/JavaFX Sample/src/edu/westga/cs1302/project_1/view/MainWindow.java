da74831a6e357a147f1668710119dcd43a5e9809
JarethUWG <jbatty1@my.westga.edu>
2024-09-23 18:44:28 -0400

    Connect UI to Business Logic for Adding Food
    Connected UI to the Food data class to allow for foods to be created and seen in the list view.

--- code/Project 1/JavaFX Sample/src/edu/westga/cs1302/project_1/view/MainWindow.java
+++ code/Project 1/JavaFX Sample/src/edu/westga/cs1302/project_1/view/MainWindow.java
@@ -1,11 +1,12 @@
 package edu.westga.cs1302.project_1.view;
 
+import edu.westga.cs1302.project_1.model.Food;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
-
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 
 import javafx.fxml.FXML;
@@ -21,14 +22,39 @@ public class MainWindow {
 	    private TextField name;
 
 	    @FXML
-	    private ListView<?> pantry;
+	    private ListView<Food> pantry;
 
 	    @FXML
-	    private ComboBox<?> type;
+	    private ComboBox<String> type;
 
 	    @FXML
 	    void addFood(ActionEvent event) {
+	    	String foodName = this.name.getText();
+	    	String foodType = this.type.getValue();
+	    	try {
+	    		Food food = new Food(foodName, foodType);
+	        	this.pantry.getItems().add(food);
+	    	} catch (IllegalArgumentException error) {
+	    		if (this.name.getText() == null || this.name.getText().isBlank() && this.type.getValue() == null) {
+	    			Alert errorPopup = new Alert(AlertType.ERROR);
+	        		errorPopup.setContentText("Unable to add food: " + error.getMessage() + " Please add a name and type for the food you wish to add.");
+	        		errorPopup.showAndWait();
+	    		} else if (this.name.getText() == null || this.name.getText().isBlank()) {
+	    			Alert errorPopup = new Alert(AlertType.ERROR);
+	        		errorPopup.setContentText("Unable to add food: " + error.getMessage() + " Please add a name for the food you wish to add.");
+	        		errorPopup.showAndWait();
+	    		}  else if (this.type.getValue() == null) {
+	    			Alert errorPopup = new Alert(AlertType.ERROR);
+	        		errorPopup.setContentText("Unable to add food: " + error.getMessage() + " Please add a type for the food you wish to add.");
+	        		errorPopup.showAndWait();
+	    		}
+	    	}
 	    	
+	    }
+	    
+	    @FXML
+	    void initialize() {
+	    	this.type.getItems().addAll("Vegetable", "Meat", "Bread", "Fruit", "Dessert", "Ingredient");
 	    }
 
 }
